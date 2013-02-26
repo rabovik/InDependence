a02d35000badda612b4bc2aba79a284222d90729
Yan Rabovik <yan@rabovik.ru>
2013-02-26 10:57:25 +0400

    project template

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Yan Rabovik on 26.02.13.
+//  Copyright (c) 2013 Yan Rabovik. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
---
+++ Tests/Tests.m
@@ -0,0 +1,32 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Yan Rabovik on 26.02.13.
+//  Copyright (c) 2013 Yan Rabovik. All rights reserved.
+//
+
+#import "Tests.h"
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Tests");
+}
+
+@end

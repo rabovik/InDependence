bbcd138e74061ab71723c17123a0cdae4c744d67
Yan Rabovik <yan@rabovik.ru>
2013-03-27 19:26:28 +0400

    Test for custom class initializer

---
+++ Tests/INDCustomClassInitializerTests.m
@@ -0,0 +1,40 @@
+//
+//  INDCustomClassInitializerTests.m
+//  InDependence
+//
+//  Created by Yan Rabovik on 27.03.13.
+//  Copyright (c) 2013 Yan Rabovik. All rights reserved.
+//
+
+#import "INDCustomClassInitializerTests.h"
+#import "InDependence.h"
+
+@implementation ColoredPaper
+independence_class_initializer(redPaper);
++(id)redPaper{
+    ColoredPaper *paper = [[self class] new];
+    paper.color = [UIColor redColor];
+    return paper;
+}
+
+@end
+
+@implementation INDCustomClassInitializerTests
+
+-(void)setUp{
+    [super setUp];
+}
+
+-(void)tearDown{
+    [INDInjector setSharedInjector:nil];
+    [super tearDown];
+}
+
+-(void)testCustomClassInitializer{
+    ColoredPaper *paper = [[INDInjector sharedInjector] getObject:[ColoredPaper class]
+                                                           parent:nil];
+    STAssertNotNil(paper, @"");
+    STAssertEqualObjects(paper.color, [UIColor redColor], @"");
+}
+
+@end
